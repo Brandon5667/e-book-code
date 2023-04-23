5ea4b969f7ac6408e3e162baef1a6e2cd21744a0
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-04-23 16:56:56 -0400

    finished model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,6 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+    }
     // define columns
   },
   {
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+    },
+    price: {
+      type: DataType.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      defaultValues: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataType.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
     // define columns
   },
   {
